3c06cc239d9d994de08f5973af02f758e94dcf70
Joe Flanagan <josephptflanagan@gmail.com>
2021-08-20 11:03:26 -0500

    added roman to int algorithm

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,7 +57,7 @@ REVERSE INTEGER ALGORITHM END*/
 
 /*<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>*/
 
-/* IS PALINDROME ALGORITHM START*/
+/* IS PALINDROME ALGORITHM START
 
 let isPalindrome = function(x) {
     
@@ -78,6 +78,58 @@ let x = 10;
 answerExplainationEl.textContent = "The given number a palindrome:"
 answerEl.textContent = isPalindrome(x);
 
-/* IS PALINDROME ALGORITHM END*/
+IS PALINDROME ALGORITHM END*/
+
+/*<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>*/
+
+/* ROMAN TO INTEGER START */
+
+let romanToInt = function(s) {
+    
+let intS = 0;
+let dict = {'I':1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M' : 1000};
+
+for (i=0;i<s.length;i++){
+    if(s[i] == 'I' || s[i] == 'X' || s[i] == 'C'){
+        if(i == s.length - 1 || s[i+1] == s[i]){
+            intS += dict[s[i]]
+        }
+        else{
+            if (s[i] == 'I'){
+                if(s[i+1] == 'V' || s[i+1] == 'X'){
+                    intS -= dict[s[i]];
+                }
+            }else if (s[i] == 'X'){
+                if(s[i+1] == 'L' || s[i+1] == 'C'){
+                    intS -= dict[s[i]];
+                }
+                else{
+                    intS += dict[s[i]];
+                }
+            }else if (s[i] == 'C'){
+                if(s[i+1] == 'D' || s[i+1] == 'M'){
+                    intS -= dict[s[i]];
+                }
+                else{
+                    intS += dict[s[i]];
+                }
+            }
+        }    
+    }
+    else{
+        intS += dict[s[i]];
+    }
+}
+
+return intS;
+
+};
+
+let s = 'MCMXC';
+
+answerExplainationEl.textContent = "The integer form of the given roman numeral is: "
+answerEl.textContent = romanToInt(s);
+
+/* ROMAN TO INTEGER END */
 
 /*<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>*/
